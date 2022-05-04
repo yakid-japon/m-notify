c99cc205287129f1b5cc3faa571be22d0cff17be
Chabla <chabijeanbaptiste@gmail.com>
2022-05-04 09:39:26 +0000

    notify

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+gem 'slack-notify'
+gem 'clockwork'
+gem 'puma'
+gem 'dotenv'
+gem 'time'
---
+++ sample.rb
@@ -0,0 +1,25 @@
+require "slack-notify"
+require 'dotenv'
+require 'clockwork'
+require 'active_support/time'
+include Clockwork
+
+Dotenv.load('./.env')
+
+module Clockwork
+  handler do |job|
+    puts "Running #{job}"
+  end
+end
+
+every(3.minutes, 'job') do
+  client = SlackNotify::Client.new(
+    webhook_url: ENV["WEBHOOK_URL"],
+    channel: "#test",
+    username: "mybot",
+    icon_url: "http://mydomain.com/myimage.png",
+    icon_emoji: ":shipit:",
+    link_names: 1
+    )
+  client.notify("Hello There!!!!!!!")
+end
